a6cac9b01ac1bf7a1e92c7a2ad292564b97c2f43
vasilich99 <oleg_kovtkov@mail.ru>
2023-10-04 22:34:39 +0200

    feat: tried first settings

---
+++ src/App.js
@@ -0,0 +1,2 @@
+import styles from './App.css';
+console.log("Hello World!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,70 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
-
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-
+const stylesHandler = MiniCssExtractPlugin.loader;
 
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  context: path.join(__dirname, '/src'),
+  entry: {
+    main: './App.js'
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts/[name].[contenthash].js',
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    hot: true,
+  },
+  plugins: [new MiniCssExtractPlugin(), ...['index'].map((file) => (
+    new HtmlWebpackPlugin({
+      inject: 'head',
+      template: `./pages/${file}.html`,
+      filename: `${file}.html`,
+    })
+  ))],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico|avif|mp3)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'assets/img/[name].[hash].[ext]',
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
+      }
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, 'css-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        config.plugins.push(new MiniCssExtractPlugin());
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+module.exports = config;
